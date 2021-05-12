72e95d62a1dc23c9a567e77be13b3f7076b8b632
Nils Pukropp <nils.pukropp@outlook.de>
2021-05-12 16:49:58 +0200

    added test for basic functions

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,27 @@ pub mod ytd;
 
 #[cfg(test)]
 mod tests {
+    use regex::Regex;
+    use std::env;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn version() {
+        let ytd = crate::ytd::YoutubeDL::new(
+            env::current_dir()
+                .expect("couldn't get working directory")
+                .to_str()
+                .unwrap(),
+            // get youtube-dl version
+            vec![crate::ytd::Arg::new("--version")],
+            String::new(),
+        )
+        .expect("Couldn't create youtube-dl");
+
+        let regex = Regex::new(r"\d{4}\.\d{2}\.\d{2}").unwrap();
+        let output = ytd.download();
+
+        // check output
+        // fails if youtube-dl is not installed
+        assert!(regex.is_match(output.output()));
     }
 }
